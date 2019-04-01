bba04a1a48dc30f483f3042a414ea2bb661cebe5
Carolina <carolsottovia@gmail.com>
2019-04-01 15:23:04 +0200

    Add Board

--- src/App.js
+++ src/App.js
@@ -14,9 +14,28 @@ class App extends Component {
           <Title content="Some Simple Title" />
           <LightSwitch />
           </main>
-          <p>
-            Edit <code>src/App.js</code> JUMP.
-          </p>
+          <h1>User Board</h1>
+          <div>
+            <div>User: <b>Carol</b></div>
+            <p>"Carol is"<b>not</b>"present"</p>
+            <button>Toggle</button>
+            <div>User: <b>Bruno</b></div>
+            <p>"Bruno is present"</p>
+            <button>Toggle</button>
+            <div>User: <b>Rodrigo</b></div>
+            <p>"Rodrigo is present"</p>
+            <button>Toggle</button>
+            <div>User: <b>Celia</b></div>
+            <p>"Celia is"<b>not</b>"present"</p>
+            <button>Toggle</button>
+            <div>User: <b>Aureliano</b></div>
+            <p>"Aureliano is present"</p>
+            <button>Toggle</button>
+            <div>User: <b>Marilena</b></div>
+            <p>"Marilena is"<b>not</b>"present"</p>
+            <button>Toggle</button>
+            {/* Edit <code>src/App.js</code> JUMP. */}
+          </div>
           <h2>Welcome to Codaisseur</h2>
           <a
             className="App-link"
