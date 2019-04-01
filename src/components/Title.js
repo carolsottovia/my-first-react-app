6798db6d1501ff942945350c877e682c94d32510
Carolina <carolsottovia@gmail.com>
2019-04-01 13:59:50 +0200

    changes on Title

--- src/components/Title.js
+++ src/components/Title.js
@@ -2,13 +2,8 @@ import React, { Component } from 'react'
 
 class Title extends Component {
   render() {
-    return 'test'
-  }
-}
-
-
-export default class Title extends Component {
-  render() {
     return <h1>{ this.props.content }</h1>
   }
 }
+
+export default Title;
